d75c4f42afc98dfaeb0b9c9ae2f3c347356b5125
Doryam <rizo0655@gmail.com>
2024-09-09 18:08:52 -0600

    Boton flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
+import { AddOutlined, Opacity } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
@@ -12,7 +14,20 @@ export const JournalPage = () => {
 
       {/* <NothingSelectedView /> */}
       <NoteView />
-      {/* NoteView */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ size: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
